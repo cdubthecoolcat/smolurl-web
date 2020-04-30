8c45685e9308984c567abfda5e03177d6c954517
Connor Wong <cdubthecoolcat@gmail.com>
2020-04-29 18:22:22 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeAppBar from './HomeAppBar';
+import UrlInput from './UrlInput';
+
+function App() {
+  return (
+    <div>
+      <HomeAppBar />
+      <UrlInput />
+    </div>
+  );
+}
+
+export default App;
